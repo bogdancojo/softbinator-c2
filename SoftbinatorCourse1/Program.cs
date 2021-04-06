a45be0d9749c3c02c31e2b2183daea8286aafbd2
bogdancojo <bogdan.cojocaru97@icloud.com>
2021-04-06 16:59:05 +0300

    Changed hello message

--- SoftbinatorCourse1/Program.cs
+++ SoftbinatorCourse1/Program.cs
@@ -6,7 +6,7 @@ namespace SoftbinatorCourse1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Softbinator!");
         }
     }
 }
